cb1ae394a56242e423a5afb642398425df34f09b
Stéphane Souron <stef@vertcitron.fr>
2019-10-24 16:43:58 +0200

    added html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 class Test {
   constructor () {
-    this.element = document.createElement('h1')
+    this.element = document.createElement('h3')
     this.element.textContent = 'Test !!!'
+    console.log('Test constructor.')
   }
 
   render () {
+    console.log('Test rendered.')
     document.body.innerHTML = ''
     document.body.appendChild(this.element)
     setTimeout(() => {
+      console.log('Test modified.')
       this.element.textContent = 'Test (deprecated) !!!'
     }, 3000)
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Citrus UI',
+      template: './src/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false
+      }
+    })
+  ]
 }
